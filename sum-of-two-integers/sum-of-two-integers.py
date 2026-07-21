6cccc72837ee5e7c0876a4f6c84b1c2b225c9a3d
Arpit <arpitsaini9831@gmail.com>
2026-07-21 12:28:45 +0530

    Add solution for Sum Of Two Integers

---
+++ sum-of-two-integers/sum-of-two-integers.py
@@ -0,0 +1,7 @@
+// LeetCode Solution: Sum Of Two Integers
+// Submitted: 2026-07-21T06:58:44.549Z
+// Language: Python3
+
+class Solution:
+    def getSum(self, a: int, b: int) -> int:
+        return a + b
