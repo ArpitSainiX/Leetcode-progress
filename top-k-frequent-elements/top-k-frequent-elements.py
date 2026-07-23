4b49bcbed95157e1fa8f5036efb247bcc7abf9ac
Arpit <arpitsaini9831@gmail.com>
2026-07-23 12:46:39 +0530

    Add solution for Top K Frequent Elements

--- top-k-frequent-elements/top-k-frequent-elements.py
+++ top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,26 +1,62 @@
 // LeetCode Solution: Top K Frequent Elements
-// Submitted: 2026-07-23T06:16:25.465Z
+// Submitted: 2026-07-23T07:16:36.169Z
 // Language: Python3
 
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        nums.sort()
         if len(nums) == 1:
             return nums
-        hs = {}
+        #bucket sort
+        count = {}
+        freq = [[] for i in range(len(nums)+1)]
 
-        for el in nums:
-            if el in hs:
-                hs[el] += 1
-            else:
-                hs[el] = 1
+        for n in nums:
+            count[n] = 1 + count.get(n, 0)
+        
+        for num, v in count.items():
+            freq[v].append(num)
+        
         res = []
+        for i in range(len(nums)-1, 0, -1):
+            for n in freq[i]:
+                res.append(n)
+                if len(res) == k:
+                    return res
 
-        for num in hs:
-            res.append([hs[num], num])
-        res.sort(reverse=True)
 
-        final_ans = []
-        for i in range(k):
-            final_ans.append(res[i][1])
-        return final_ans
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # nums.sort()
+        # if len(nums) == 1:
+        #     return nums
+        # hs = {}
+
+        # for el in nums:
+        #     if el in hs:
+        #         hs[el] += 1
+        #     else:
+        #         hs[el] = 1
+        # res = []
+
+        # for num in hs:
+        #     res.append([hs[num], num])
+        # res.sort(reverse=True)
+
+        # final_ans = []
+        # for i in range(k):
+        #     final_ans.append(res[i][1])
+        # return final_ans
