d2a1868a4f372f68f93645ed037840ea4748eb1a
Arpit <arpitsaini9831@gmail.com>
2026-07-25 15:38:40 +0530

    Add solution for Palindrome Linked List

--- palindrome-linked-list/palindrome-linked-list.py
+++ palindrome-linked-list/palindrome-linked-list.py
@@ -1,5 +1,5 @@
 // LeetCode Solution: Palindrome Linked List
-// Submitted: 2026-07-25T10:06:40.533Z
+// Submitted: 2026-07-25T10:08:35.777Z
 // Language: Python3
 
 # Definition for singly-linked list.
@@ -9,23 +9,39 @@
 #         self.next = next
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
-        reverse_arr = []
-        arr = []
+        lst = []
         temp = head
         while temp:
-            arr.append(temp.val)
-            reverse_arr.append(temp.val)
+            lst.append(temp.val)
             temp = temp.next
+        return lst == lst[::-1]        
+
+
+
+
+
+
+
+
+
+        #worst approach
+        # reverse_arr = []
+        # arr = []
+        # temp = head
+        # while temp:
+        #     arr.append(temp.val)
+        #     reverse_arr.append(temp.val)
+        #     temp = temp.next
         
 
-        s1 = ""
-        s2 = ""
-        for i in range(len(arr)):
-            s1 += str(arr[i])
+        # s1 = ""
+        # s2 = ""
+        # for i in range(len(arr)):
+        #     s1 += str(arr[i])
         
-        for j in range(len(reverse_arr)-1,-1,-1):
-            s2 += str(reverse_arr[j])
+        # for j in range(len(reverse_arr)-1,-1,-1):
+        #     s2 += str(reverse_arr[j])
         
-        if s1 == s2:
-            return True
-        return False
+        # if s1 == s2:
+        #     return True
+        # return False
