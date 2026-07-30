cc615d22a1041cc123bc9177c16cc9cbc0e56881
Arpit <arpitsaini9831@gmail.com>
2026-07-30 12:53:58 +0530

    Add solution for Minimum Number Of Pushes To Type Word I

--- minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.py
+++ minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.py
@@ -1,15 +1,10 @@
 // LeetCode Solution: Minimum Number Of Pushes To Type Word I
-// Submitted: 2026-07-30T07:19:50.506Z
+// Submitted: 2026-07-30T07:23:49.618Z
 // Language: Python3
 
 class Solution:
     def minimumPushes(self, word: str) -> int:
-        if len(word) < 8:
-            return len(word)
-        else:
-            extra_letters = len(word)-8
-            # return len(word) + extra_letters
-            if len(word) % 2 == 0:
-                return len(word)+ extra_letters
-            else:
-                return len(word)+ extra_letters+1
+        pushes = 0
+        for i in range(len(word)):
+            pushes += (i//8) + 1
+        return pushes
