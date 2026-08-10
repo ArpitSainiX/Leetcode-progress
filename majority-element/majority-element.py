80facbf42c1551bb0f695e5dbcda31145cf24053
Arpit <arpitsaini9831@gmail.com>
2026-08-10 17:26:12 +0530

    Add solution for Majority Element

--- majority-element/majority-element.py
+++ majority-element/majority-element.py
@@ -1,16 +1,18 @@
 // LeetCode Solution: Majority Element
-// Submitted: 2026-08-10T11:55:34.735Z
+// Submitted: 2026-08-10T11:56:09.311Z
 // Language: Python3
 
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        hs = {}
-        for el in nums:
-            if el in hs:
-                hs[el] += 1
-            else:
-                hs[el] = 1
+        nums.sort()
+        return nums[len(nums)//2]
+        # hs = {}
+        # for el in nums:
+        #     if el in hs:
+        #         hs[el] += 1
+        #     else:
+        #         hs[el] = 1
 
-        for k, v in hs.items():
-            if v > len(nums)//2:
-                return k
+        # for k, v in hs.items():
+        #     if v > len(nums)//2:
+        #         return k
