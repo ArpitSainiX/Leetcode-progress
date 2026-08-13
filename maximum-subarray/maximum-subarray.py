8fb953e1b5ccf56567449a4c24a299019800064a
Arpit <arpitsaini9831@gmail.com>
2026-08-13 21:26:10 +0530

    Add solution for Maximum Subarray

--- maximum-subarray/maximum-subarray.py
+++ maximum-subarray/maximum-subarray.py
@@ -1,13 +1,13 @@
 // LeetCode Solution: Maximum Subarray
-// Submitted: 2026-08-13T15:55:15.780Z
+// Submitted: 2026-08-13T15:56:08.778Z
 // Language: Python3
 
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        maxSum = 0
-        curr_sum = 0
+        maxSum = curr_sum = nums[0]
+        
 
-        for i in range(len(nums)):
+        for i in range(1,len(nums)):
             curr_sum = max(nums[i], nums[i]+curr_sum)
             maxSum = max(maxSum, curr_sum)
         return maxSum
