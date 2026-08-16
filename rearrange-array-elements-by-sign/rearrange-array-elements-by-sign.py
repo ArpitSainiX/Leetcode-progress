7e9d28a24f0152ed546ba2a3c316beac711107e2
Arpit <arpitsaini9831@gmail.com>
2026-08-16 10:48:45 +0530

    Add solution for Rearrange Array Elements By Sign

--- rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.py
+++ rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.py
@@ -1,23 +1,52 @@
 // LeetCode Solution: Rearrange Array Elements By Sign
-// Submitted: 2026-08-16T05:15:09.405Z
+// Submitted: 2026-08-16T05:18:44.476Z
 // Language: Python3
 
 class Solution:
     def rearrangeArray(self, nums: List[int]) -> List[int]:
-        pos, neg = [], []
+        pos, neg, res = [],[],[]
 
-        for el in nums:
-            if el > 0:
-                pos.append(el) #Array of positive elements.
+        for i in nums:
+            if i > 0:
+                pos.append(i)
             else:
-                neg.append(el) #Array of negaive elements.
+                neg.append(i)
         
-        res = []
-        l,r = 0,0
-
-        while l < len(pos) and r < len(neg):
-            res.append(pos[l])
-            res.append(neg[r])
-            l += 1
-            r += 1
+        res = [0]*(len(nums))
+        res[0::2] = pos
+        res[1::2] = neg
         return res
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # pos, neg = [], []
+
+        # for el in nums:
+        #     if el > 0:
+        #         pos.append(el) #Array of positive elements.
+        #     else:
+        #         neg.append(el) #Array of negaive elements.
+        
+        # res = []
+        # l,r = 0,0
+
+        # while l < len(pos) and r < len(neg):
+        #     res.append(pos[l])
+        #     res.append(neg[r])
+        #     l += 1
+        #     r += 1
+        # return res
