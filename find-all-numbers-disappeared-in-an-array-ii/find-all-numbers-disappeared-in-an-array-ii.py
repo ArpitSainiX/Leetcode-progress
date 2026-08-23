ee6b565cebde95ce46d35c93a6be471286c2e949
Arpit <arpitsaini9831@gmail.com>
2026-08-23 15:57:19 +0530

    Add solution for Find All Numbers Disappeared In An Array Ii

--- find-all-numbers-disappeared-in-an-array-ii/find-all-numbers-disappeared-in-an-array-ii.py
+++ find-all-numbers-disappeared-in-an-array-ii/find-all-numbers-disappeared-in-an-array-ii.py
@@ -1,5 +1,5 @@
 // LeetCode Solution: Find All Numbers Disappeared In An Array Ii
-// Submitted: 2026-08-23T10:22:52.152Z
+// Submitted: 2026-08-23T10:27:13.703Z
 // Language: Python3
 
 class Solution:
@@ -7,20 +7,18 @@ class Solution:
         nums.sort()
 
         res = []
-        elem = []
+        curr = lower
 
-        for i in range(lower, upper+1):
-            elem.append(i)
-        
-        l = 0 
-        while l < len(elem):
-            if elem[l] in nums:
-                l += 1
+        for num in nums:
+            if num < curr:
                 continue
             
-            r = l
-            while r < len(elem) and elem[r] not in nums:
-                r += 1
-            res.append([elem[l], elem[r-1]])
-            l = r
+            if num > curr:
+                res.append([curr, min(num-1, upper)])
+            curr = num + 1
+
+            if curr > upper:
+                break
+        if curr <= upper:
+            res.append([curr, upper])
         return res
