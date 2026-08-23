ba6d1d921ecde349220e771c21e6cb8d02e647ec
Arpit <arpitsaini9831@gmail.com>
2026-08-23 21:43:35 +0530

    Add solution for Sum Game

--- sum-game/sum-game.py
+++ sum-game/sum-game.py
@@ -1,5 +1,5 @@
 // LeetCode Solution: Sum Game
-// Submitted: 2026-08-23T16:13:19.803Z
+// Submitted: 2026-08-23T16:13:29.737Z
 // Language: Python3
 
 class Solution:
@@ -8,7 +8,7 @@ class Solution:
         half = n // 2
 
         lsum = sum(int(c) for c in num[:half] if c != "?")
-        rsum = sum(int(c) for c in numl[half:] if c != "?")
+        rsum = sum(int(c) for c in num[half:] if c != "?")
 
 
         lCount = num[:half].count("?")
