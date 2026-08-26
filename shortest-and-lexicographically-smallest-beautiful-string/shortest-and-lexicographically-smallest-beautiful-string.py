f5d24db6ef1f07682d347af9ddc40825e6da9e59
Arpit <arpitsaini9831@gmail.com>
2026-08-26 14:46:54 +0530

    Add solution for Shortest And Lexicographically Smallest Beautiful String

--- shortest-and-lexicographically-smallest-beautiful-string/shortest-and-lexicographically-smallest-beautiful-string.py
+++ shortest-and-lexicographically-smallest-beautiful-string/shortest-and-lexicographically-smallest-beautiful-string.py
@@ -1,25 +1,30 @@
 // LeetCode Solution: Shortest And Lexicographically Smallest Beautiful String
-// Submitted: 2026-08-26T09:05:12.160Z
+// Submitted: 2026-08-26T09:16:45.574Z
 // Language: Python3
 
 class Solution:
     def shortestBeautifulSubstring(self, s: str, k: int) -> str:
-        #edge case:
-        if len(s) <= 2:
+        if s.count("1") < k:
             return ""
-        
-        #where the actual algorithm will start.
-        l,r = 0,2
-        req_str = ""
-        while l < r and r < len(s):
-            subStr = s[l:r+1] #making the substr
-            count = subStr.count("1") #counting 1's in subStr.
-            
-            if count < k:
-                r += 1
-            elif count > k:
+
+        ans = ""
+        l = 0
+        ones = 0
+
+        for r in range(len(s)):
+            if s[r] == "1":
+                ones += 1
+            while ones == k:
+                
+                while s[l] == "0":
+                    l += 1
+
+                subStr = s[l : r + 1]
+
+                if not ans or len(subStr) < len(ans) or (len(subStr) == len(ans) and subStr < ans):
+                    ans = subStr
+                if s[l] == "1":
+                    ones -= 1
                 l += 1
-            elif count == k:
-                req_str = subStr
-                l += 1
-        return req_str
+
+        return ans
