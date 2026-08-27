60ba93f1ae00ae16cf640137e4ba9938c2a338fb
Arpit <arpitsaini9831@gmail.com>
2026-08-27 12:17:34 +0530

    Add solution for Subarray Sum Equals K

--- subarray-sum-equals-k/subarray-sum-equals-k.py
+++ subarray-sum-equals-k/subarray-sum-equals-k.py
@@ -1,18 +1,37 @@
 // LeetCode Solution: Subarray Sum Equals K
-// Submitted: 2026-08-17T10:02:52.431Z
+// Submitted: 2026-08-27T06:47:33.672Z
 // Language: Python3
 
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        freq = {0:1}
-        currSum = 0
+        curr_sum = 0
         count = 0
-        for num in nums:
-            currSum += num
-            if (currSum - k) in freq:
-                count += freq[currSum-k]
-            if currSum in freq:
-                freq[currSum] += 1
-            else:
-                freq[currSum] = 1
+
+        l,r = 0,1
+
+        while l <= r and r < len(nums):
+            subarr = nums[l:r+1]
+            curr_sum = sum(subarr)
+
+            #checking conditions.
+            if curr_sum > k:
+                curr_sum -= nums[l]
+                l += 1
+            elif curr_sum < k:
+                r += 1
+            elif curr_sum == k:
+                count += 1
+                r += 1
         return count
+        # freq = {0:1}
+        # currSum = 0
+        # count = 0
+        # for num in nums:
+        #     currSum += num
+        #     if (currSum - k) in freq:
+        #         count += freq[currSum-k]
+        #     if currSum in freq:
+        #         freq[currSum] += 1
+        #     else:
+        #         freq[currSum] = 1
+        # return count
