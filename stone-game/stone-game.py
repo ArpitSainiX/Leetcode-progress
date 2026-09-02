fd6aabec82bc208f2d658aed603800b90100244b
Arpit <arpitsaini9831@gmail.com>
2026-09-02 13:56:14 +0530

    Add solution for Stone Game

---
+++ stone-game/stone-game.py
@@ -0,0 +1,7 @@
+// LeetCode Solution: Stone Game
+// Submitted: 2026-09-02T08:26:12.597Z
+// Language: Python3
+
+class Solution:
+    def stoneGame(self, piles: List[int]) -> bool:
+        return True
