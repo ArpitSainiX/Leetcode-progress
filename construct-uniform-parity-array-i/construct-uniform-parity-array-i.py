6d597d16264fc062dc997b50494d6ec5caa1638a
Arpit <arpitsaini9831@gmail.com>
2026-09-02 13:54:53 +0530

    Add solution for Construct Uniform Parity Array I

---
+++ construct-uniform-parity-array-i/construct-uniform-parity-array-i.py
@@ -0,0 +1,7 @@
+// LeetCode Solution: Construct Uniform Parity Array I
+// Submitted: 2026-09-02T08:24:50.612Z
+// Language: Python3
+
+class Solution:
+    def uniformArray(self, nums1: list[int]) -> bool:
+        return True
