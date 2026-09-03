3f582aa3da246388d308e2b1b4f5e0c659b1d0e1
Arpit <arpitsaini9831@gmail.com>
2026-09-03 21:35:12 +0530

    Add solution for Search In Rotated Sorted Array Ii

---
+++ search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.py
@@ -0,0 +1,9 @@
+// LeetCode Solution: Search In Rotated Sorted Array Ii
+// Submitted: 2026-09-03T16:05:08.045Z
+// Language: Python3
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> bool:
+        if target in nums:
+            return True
+        return False
