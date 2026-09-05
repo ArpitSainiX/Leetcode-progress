a0757cbfe805b89dc5cd576a5dfef923c18885d7
Arpit <arpitsaini9831@gmail.com>
2026-09-05 14:16:41 +0530

    Add solution for Smallest Stable Index Ii

--- smallest-stable-index-ii/smallest-stable-index-ii.py
+++ smallest-stable-index-ii/smallest-stable-index-ii.py
@@ -1,18 +1,25 @@
 // LeetCode Solution: Smallest Stable Index Ii
-// Submitted: 2026-09-05T08:30:05.965Z
+// Submitted: 2026-09-05T08:46:36.778Z
 // Language: Python3
 
 class Solution:
     def firstStableIndex(self, nums: list[int], k: int) -> int:
         n = len(nums)
-        res = []
-        for i in range(len(nums)):
-            max_val = max(nums[:i+1])
-            min_val = min(nums[i:n])
+        suffMin = []*n 
+        prefMax = []*n
 
-            instability = max_val - min_val
-            if instability <= k:
-                res.append(i)
+
+        for i in range(n):
+            min_val = min(nums[i:n])
+            max_val = max(nums[:i+1])
+            suffMin.append(min_val)
+            prefMax.append(max_val)
+
+        res = []
+        for j in range(n):
+            subtract = prefMax[i] - suffMin[i]
+            if subtract <= k:
+                res.append(i) 
         
         if len(res) != 0:
             return min(res)
