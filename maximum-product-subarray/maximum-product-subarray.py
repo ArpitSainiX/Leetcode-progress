3053b209985dd428ec80270230af9fec3d4dbae3
Arpit <arpitsaini9831@gmail.com>
2026-09-10 12:39:08 +0530

    Add solution for Maximum Product Subarray

--- maximum-product-subarray/maximum-product-subarray.py
+++ maximum-product-subarray/maximum-product-subarray.py
@@ -1,5 +1,5 @@
 // LeetCode Solution: Maximum Product Subarray
-// Submitted: 2026-09-10T07:00:00.400Z
+// Submitted: 2026-09-10T07:09:01.067Z
 // Language: Python3
 
 class Solution:
@@ -10,18 +10,33 @@ class Solution:
         '''
             if elements multiplication is less than 0 then we need to remove element from the left side.
         '''
-        n = len(nums)
-        pre = suff = 1
-        ans = float('-inf')
+        # n = len(nums)
+        # pre = suff = 1
+        # ans = float('-inf')
 
-        for i in range(len(nums)):
-            if pre == 0:
-                pre = 1
-            if suff == 0:
-                suff = 1 
+        # for i in range(len(nums)):
+        #     if pre == 0:
+        #         pre = 1
+        #     if suff == 0:
+        #         suff = 1 
             
-            pre *= nums[i]
-            suff *= nums[n-i-1]
+        #     pre *= nums[i]
+        #     suff *= nums[n-i-1]
 
-            ans = max(ans, pre, suff)
+        #     ans = max(ans, pre, suff)
+        # return ans
+
+        '''second optimal appraoch'''
+        ans = maxProd = minProd = nums[0]
+
+        for i in range(1, len(nums)):
+            curr = nums[i]
+
+            if curr < 0:
+                maxProd, minProd = minProd, maxProd
+            
+            maxProd = max(curr, maxProd * curr)
+            minProd = max(curr, minProd * curr)
+
+            ans = max(ans, maxProd, minProd)
         return ans
