085b5ecbfe46b7d3db0dc1df65c15353633f4d7d
Arpit <arpitsaini9831@gmail.com>
2026-09-16 21:05:43 +0530

    Add solution for Largest Odd Number In String

--- largest-odd-number-in-string/largest-odd-number-in-string.py
+++ largest-odd-number-in-string/largest-odd-number-in-string.py
@@ -1,23 +1,39 @@
 // LeetCode Solution: Largest Odd Number In String
-// Submitted: 2026-09-16T15:32:55.409Z
+// Submitted: 2026-09-16T15:35:42.684Z
 // Language: Python3
 
 class Solution:
     def largestOddNumber(self, s: str) -> str:
-        i = 0
-        while s[i] == "0":
-            i += 1
-        
-        new_s = s[i: ] #example -> 32579
-        r = len(new_s)-1 # pointing it to the last
+        if s in "13579":
+            return s
+        else:
+            for i in range(len(s)-1, -1, -1):
+                if s[i] in "13579":
+                    return s[:i+1]
+            return ""
 
-        #currently i is at index 2
-        while r >= i:
-            if checkOdd(new_s[:r+1]):
-                return new_s[:r+1]
-            else:
-                r -= 1 
-        return ""
+
+
+
+
+
+
+
+
+        # i = 0
+        # while s[i] == "0":
+        #     i += 1
+        
+        # new_s = s[i: ] #example -> 32579
+        # r = len(new_s)-1 # pointing it to the last
+
+        # #currently i is at index 2
+        # while r >= i:
+        #     if checkOdd(new_s[:r+1]):
+        #         return new_s[:r+1]
+        #     else:
+        #         r -= 1 
+        # return ""
 
 
 
