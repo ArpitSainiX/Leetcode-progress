f3a652aa8b2156e28ee849f4d6443bf7a2d36e4b
Arpit <arpitsaini9831@gmail.com>
2026-09-21 16:55:52 +0530

    Add solution for Group Anagrams

--- group-anagrams/group-anagrams.py
+++ group-anagrams/group-anagrams.py
@@ -1,11 +1,16 @@
 // LeetCode Solution: Group Anagrams
-// Submitted: 2026-09-21T11:24:43.492Z
+// Submitted: 2026-09-21T11:25:48.467Z
 // Language: Python3
 
 class Solution:
     def groupAnagrams(self, strs: list[str]) -> list[list[str]]:
         groups = defaultdict(list)
 
+        # for s in strs:
+        #     groups["".join(sorted(s))].append(s)
+        # return list(groups.values())
+
         for s in strs:
-            groups["".join(sorted(s))].append(s)
+            key = "".join(sorted(s))
+            groups[key].append(s)
         return list(groups.values())
