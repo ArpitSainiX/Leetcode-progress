1586bccd0c9f9b18739a678b58a77639f0fb2256
Arpit <arpitsaini9831@gmail.com>
2026-09-22 13:26:17 +0530

    Add solution for Permutation Sequence

--- permutation-sequence/permutation-sequence.py
+++ permutation-sequence/permutation-sequence.py
@@ -1,23 +1,42 @@
 // LeetCode Solution: Permutation Sequence
-// Submitted: 2026-09-22T06:10:33.138Z
+// Submitted: 2026-09-22T07:56:12.253Z
 // Language: Python3
 
 class Solution:
     def getPermutation(self, n: int, k: int) -> str:
-        strs = []
-        for i in range(1, n+1):
-            strs += str(i)
+        strs = [str(i) for i in range(1,n+1)]
+
+        fact = [1]*n
+        for i in range(1,n):
+            fact[i] = fact[i-1]*i
         
+        k -= 1
         ans = []
-        def backtracking(curr):
-            if len(curr) == len(strs):
-                ans.append(curr.copy())
-                return
+
+        for i in range(n, 0, -1):
+            block = fact[i-1]
+            idx = k // block
+            k %= block
+            ans.append(strs.pop(idx))
+        return "".join(ans)
+
+
+
+
+        # strs = []
+        # for i in range(1, n+1):
+        #     strs += str(i)
+        
+        # ans = []
+        # def backtracking(curr):
+        #     if len(curr) == len(strs):
+        #         ans.append(curr.copy())
+        #         return
             
-            for num in strs:
-                if num not in curr:
-                    curr.append(num)
-                    backtracking(curr)
-                    curr.pop()
-        backtracking([])
-        return "".join(ans[k-1])
+        #     for num in strs:
+        #         if num not in curr:
+        #             curr.append(num)
+        #             backtracking(curr)
+        #             curr.pop()
+        # backtracking([])
+        # return "".join(ans[k-1])
