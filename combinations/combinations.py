b4d4ea1581d59696480c53771a8c89fd7ea7e307
Arpit <arpitsaini9831@gmail.com>
2026-09-22 13:57:08 +0530

    Add solution for Combinations

--- combinations/combinations.py
+++ combinations/combinations.py
@@ -1,33 +1,21 @@
 // LeetCode Solution: Combinations
-// Submitted: 2026-09-22T08:21:04.401Z
+// Submitted: 2026-09-22T08:27:04.600Z
 // Language: Python3
 
 class Solution:
     def combine(self, n: int, k: int) -> list[list[int]]:
-        digits = [i for i in range(1,n+1)]
-        ans = []
-        def backtrack(curr):
+        res = []
+        curr = []
+
+        def backtrack(st):
             if len(curr) == k:
-                ans.append(curr[:k])
+                res.append(curr[:])
                 return
             
-            for num in digits:
-                if num not in curr:
-                    curr.append(num)
-                    backtrack(curr)
-                    curr.pop()
-            
-        backtrack([])
-        res = []
-
-        for el in ans:
-            el.sort()
-            res.append(el)
-
-
-        arr = []
-        for num in res:
-            if num in arr:
-                continue
-            arr.append(num)
-        return arr
+            for i in range(st, n+1):
+                curr.append(i)
+                backtrack(i+1)
+                curr.pop()
+        backtrack(1)
+        return res
+        
